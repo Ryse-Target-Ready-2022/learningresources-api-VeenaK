1ae7432cf865791deb93a7d062172b4ad50902cc
Veena Katakol <>
2022-08-02 20:50:13 +0530

    Classes and Objects

---
+++ learningresources-api-Veena-2-ClassesAndObjects/src/test/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class LearningresourcesApiApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
